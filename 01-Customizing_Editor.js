c47de793575380b8ddceb0f724191f717a8d61b9
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-04-12 19:42:55 +0530

    Add files via upload

---
+++ 01-Customizing_Editor.js
@@ -0,0 +1,60 @@
+//----------Switch Themes----------//
+
+// There are a lot of themes to choose from. 
+// These are some of the more popular light and dark variants. 
+// The 3 in the last column are just for fun and making people laugh in presentations.
+//  All of these themes are included in the VS Code Can Do That extension pack.
+
+
+
+
+// Light Themes           Dark Themes            Fun
+
+// Hop Light              Night Owl              Slack
+
+// Night Owl Light        Cobalt2                Hot Dog Stand
+
+// Min                    Noctis                 win95
+
+// Noctis                 Dracula
+
+
+
+
+//----------Install a new icon theme----------//
+
+
+// There are two types of themes in VS Code - color themes and icon themes. Icon themes will change the icons in the explorer for File, Folders, Projects and the like.
+
+// There are two icon themes included in the VS Code Can Do That extension pack...
+
+// 1-Material Icons
+
+// 2-Chalice Icons
+
+// To switch to a new Icon Theme, open the Command Palette (Cmd/Ctrl + Shift + P) and select "Preferences: File Icon Theme"
+
+
+//----------Switch Fonts----------//
+
+
+// Switching up fonts can bring some life and energy back into your editor. 
+// You aren't locked into the default font.
+
+// Here are some fonts that I recommend, both paid and free.
+
+
+//    FREE                                       PAID
+
+//  FiraCode                                  Dank Mono
+
+//  Hasklig                                   Operator Mono
+
+//  Monoid                                    Pragmata Pro
+
+
+// After installing the font, enable it User Settings.
+
+// * Press Cmd/Ctrl + , to open the Settings Editor
+// * Search for "Font"
+// * Specify the name of the new font
