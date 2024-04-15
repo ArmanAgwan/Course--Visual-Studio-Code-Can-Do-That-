e43f0837a4d8da447750facefc466ffcea6c7989
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-04-15 10:00:52 +0530

    Add files via upload

---
+++ 02-Productivity_Tricks.js
@@ -0,0 +1,71 @@
+//                   //----------Essential navigation shortcuts----------//
+
+// VS Code has a keyboard shortcut for virtually anything that you can do with a mouse. 
+// The more of these that you can use, the faster you will be. In addition to the 5 core keyboard shortcuts from the start of this workshop, 
+// these are the essential keyboard shortcuts for navigating around the editor without a mouse.
+
+
+//      ACTION                                              SHORTCUT
+
+// Jump to previous file                                 Cmd/Ctrl + P + P
+
+// Set focus to Sidebar                                  Cmd/Ctrl + 0
+
+// Set focus to Editor                                   Cmd/Ctrl + 1
+
+// Open Explorer View                                    Cmd/Ctrl + Shift + E
+
+// Open Debug View                                       Cmd/Ctrl + Shift + D
+
+// Open Source Control View                              Ctrl + Shift + G
+
+// Open Extensions View                                  Cmd/Ctrl + Shift + X
+
+
+
+//              //----------Creating HTML with Emmet----------//
+
+
+// Web developers spend a lot of their time composing HTML. There is a feature built-in to VS Code called "Emmet".
+// Emmet helps you to quickly create markup, styles and even provides some neat utilities for automating various routine tasks.
+
+
+// :-Create HTML boilerplate:-
+
+// 1- Open the "index.html" file in the "start" folder
+
+// 2- Scaffold out a base HTML file
+
+//    1- Type ! and press tab
+
+// 3- Press tab 3 more times to place the cursor at the tab stop in the "title" attribute and entire "I Love Lamp"
+
+// 4- Press tab once more to enter the body of the page
+
+
+//               //-----Include the Bulma CSS framework-----//
+
+
+// Bulma is a great little CSS framework. We're going to use it to help us position all of
+// the elements on the page.
+
+// 1- In the head tag of the page, type link and press tab
+
+// 2- Enter "bulma.css". Bulma is included as a local file in the "start" project
+
+
+//               //----------Add some markup----------//
+
+
+// You'll see two sections of markup that are commented out - Step 1 and Step 2. You'll need to create these 
+// blocks with Emmet.
+
+// 1- Enter .has-text-centered and press tab and then enter
+
+// 2- Enter img and press tab
+
+// 3- Enter "logo.png" as the "src" of the image
+
+// 4- Add a new line after the closing </div> tag
+
+// Type .section>.columns and press tab
