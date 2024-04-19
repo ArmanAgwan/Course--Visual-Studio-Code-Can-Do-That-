fb85c093a0164836635042b57be94ce3910f3ea6
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-04-19 08:10:01 +0530

    Add files via upload

---
+++ 03-Navigation_&_Refactoring.js
@@ -0,0 +1,24 @@
+               //----------Navigation And Refactoring----------//
+
+
+VS Code contains many navigation and refactoring tools that enable you to move around your code with the greatest of ease. 
+In this exercise, you'll learn how to format, move, duplicate, delete, fold, extract, find, browse, rename, peek, extract and even how to 
+trick the TypeScript parser into checking your JavaScript for errors.
+Make sure that you run npm install on the "start" project before you being this exercise.
+
+
+              //----------Moving, Duplicating and Deleting----------//
+
+The most common thing you'll do in your editor is move, duplicate and delete lines. Historically, 
+we have done this with the use of copy/paste and the delete key when necessary. VS Code contains functionality that makes this process much more productive. 
+
+Here are a few more keyboard shortcuts to add to your repertoire. 
+
+
+  Action                                                         Keyboard Shortcut
+
+Duplicate line                                             Opt/Alt + Shift + Down/Up Arrow
+
+Move line                                                    Opt/Alt + Down/Up Arrow
+
+Delete Line                                                   Cmd/Ctrl + Shift + K
