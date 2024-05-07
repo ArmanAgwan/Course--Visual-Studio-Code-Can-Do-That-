5ad462fd62570acd608c36482d3f64bece46dde8
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-05-07 18:32:25 +0530

    Add files via upload

--- 03-Navigation_&_Refactoring.js
+++ 03-Navigation_&_Refactoring.js
@@ -1,24 +1,37 @@
-               //----------Navigation And Refactoring----------//
+//                //----------Navigation And Refactoring----------//
 
 
-VS Code contains many navigation and refactoring tools that enable you to move around your code with the greatest of ease. 
-In this exercise, you'll learn how to format, move, duplicate, delete, fold, extract, find, browse, rename, peek, extract and even how to 
-trick the TypeScript parser into checking your JavaScript for errors.
-Make sure that you run npm install on the "start" project before you being this exercise.
+// VS Code contains many navigation and refactoring tools that enable you to move around your code with the greatest of ease. 
+// In this exercise, you'll learn how to format, move, duplicate, delete, fold, extract, find, browse, rename, peek, extract and even how to 
+// trick the TypeScript parser into checking your JavaScript for errors.
+// Make sure that you run npm install on the "start" project before you being this exercise.
 
 
-              //----------Moving, Duplicating and Deleting----------//
+//               //----------Moving, Duplicating and Deleting----------//
 
-The most common thing you'll do in your editor is move, duplicate and delete lines. Historically, 
-we have done this with the use of copy/paste and the delete key when necessary. VS Code contains functionality that makes this process much more productive. 
+// The most common thing you'll do in your editor is move, duplicate and delete lines. Historically, 
+// we have done this with the use of copy/paste and the delete key when necessary. VS Code contains functionality that makes this process much more productive. 
 
-Here are a few more keyboard shortcuts to add to your repertoire. 
+// Here are a few more keyboard shortcuts to add to your repertoire. 
 
 
-  Action                                                         Keyboard Shortcut
+//   Action                                                         Keyboard Shortcut
 
-Duplicate line                                             Opt/Alt + Shift + Down/Up Arrow
+// Duplicate line                                             Opt/Alt + Shift + Down/Up Arrow
 
-Move line                                                    Opt/Alt + Down/Up Arrow
+// Move line                                                    Opt/Alt + Down/Up Arrow
 
-Delete Line                                                   Cmd/Ctrl + Shift + K
+// Delete Line                                                   Cmd/Ctrl + Shift + K
+
+
+//           //----------Move the "title" tag above the "link" tag----------//
+
+// *Put your cursor anywhere on the "title" line
+// *Press Opt/Alt + Up Arrow
+
+
+//           //----------Duplicate the "description meta tag----------//
+
+// *Put your cursor anywhere on the line with the "meta" tag that has a name of "description"
+// *Press Opt/Alt + Shift + Down Arrow
+// *Change the "name" attribute to be "og:description" 
